c913f1f54fde38f48df35a469b2fe501ca7b28cc
Art Lowel <art.lowel+gitkraken@gmail.com>
2018-06-01 12:58:36 +0200

    initial page content

--- src/app/+admin/admin.module.ts
+++ src/app/+admin/admin.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
+import { SharedModule } from '../shared/shared.module';
 import { AdminRoutingModule } from './admin-routing.module';
 import { UsersPageComponent } from './users-page/users-page.component';
 
 @NgModule({
   imports: [
-    AdminRoutingModule
+    AdminRoutingModule,
+    SharedModule
   ],
   declarations: [
     UsersPageComponent
